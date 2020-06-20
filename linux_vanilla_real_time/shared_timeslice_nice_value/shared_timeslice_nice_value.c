c0dd86b33a5e029030c4ed4085328ec91162b8dd
Jugurtha BELKALEM <jugurthabelkalem@gmail.com>
2020-06-20 14:05:45 +0200

    Fix invalid set_sched example
    Signed-off-by: Jugurtha BELKALEM <jugurthabelkalem@gmail.com>

--- linux_vanilla_real_time/shared_timeslice_nice_value/shared_timeslice_nice_value.c
+++ linux_vanilla_real_time/shared_timeslice_nice_value/shared_timeslice_nice_value.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> // Required by nice().
+#include <unistd.h> // inclure nice().
 #include <errno.h>
 
 int main(int argc, char *argv[]){
-    int nice_value = -1; // Stores process nice value.
-
+    int nice_value = -1;
     /*
-    * It is enough to pass 0 as nice value
-    * increment to get current nice value. 
+    * nice altère la priorité dynamique d'un processus
+    * à exécution temps partagés.
+    * param : Nouvelle valeur de nice (0 pour récupérer
+    *         la valeur en cours). 
     */
     nice_value = nice(0);
 
     printf ("--- Initial nice value %d ---\n", nice_value);
 
-    /*
-    * One can passe a value!=0 to nice in order to change process niceness.
-    * When setting nice with value!=0, errno variable must be
-    * Checked as -1 is also a valid return value from nice.
-    */
     errno = 0;
-    // Increase process nice value (decrease dynamic priority).
+    // Incrémenter la valeur nice à 5 (baisser la priorité dynamique).
     nice_value = nice(5);
     
+    /*
+    * La vérification de errno est requise car nice = -1
+    * représente aussi une valeur valide nice=[-20, 19].
+    */
     if(nice_value == -1 && errno != 0){
         perror ("nice error");
     } else {
         printf ("--- New nice value is : %d --\n", nice_value);
     }
-
     return EXIT_SUCCESS;
 }
 
--- linux_vanilla_real_time/shared_timeslice_policies/shared_timeslice_policies.c
+++ linux_vanilla_real_time/shared_timeslice_policies/shared_timeslice_policies.c
@@ -1,15 +1,19 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sched.h>
+#include <errno.h>
 
 void display_process_sched_attributes(){
-    int scheduler_policy = -1; // Stores scheduler policy.
+    int scheduler_policy = -1;
 
-    // Returns current process scheduler.
+    // Retourne la politique d'ordonnacement du procesus
+    // en cours.
     scheduler_policy = sched_getscheduler(0);
 
-    printf("Current scheduling policy : ");    
+    printf("Current scheduling policy : ");
+    
     switch(scheduler_policy){
         case SCHED_OTHER:
             printf("SCHED_OTHER\n");
@@ -27,8 +31,8 @@ void display_process_sched_attributes(){
             perror("sched_getscheduler error");    
         break; 
             /*
-            * Actually there 2 other policies (SCHED_FIFO
-            * and SCHED_RR) but are reserved for realtime.
+            * Il en existe 2 autres politiques (SCHED_FIFO
+            * et SCHED_RR) réservées au temps réel.
             */
         default:
             printf("Unknown\n");
@@ -38,14 +42,21 @@ void display_process_sched_attributes(){
 }
 
 int main(int argc, char *argv[]){
-
+    int t;
+    struct sched_param sched_p;
+    // Affiche la politique d'ordonnancement.
     display_process_sched_attributes();
 
+    sched_p.sched_priority = 0; // N'est pas prise en compte
+                                // dans le temps partagé
     /*
-    * sched_setscheduler can be used to change process scheduling policy.
-    * Argument param is not needed for timeshared processes (pass NULL).
-    */
-    sched_setscheduler(0, SCHED_BATCH, NULL);    
+    * sched_setscheduler change la politique d'ordonnancement.
+    * 1er param : PID du processus (0 pour processus en cours).
+    * 2ème param : type de politique d'ordonnancement
+    * 3ème param : paramètre d'ordonnancement.
+    */ 
+    if(sched_setscheduler(0, SCHED_BATCH, &sched_p) == -1)
+        perror ("sched_setscheduler ");
 
     display_process_sched_attributes();
 
